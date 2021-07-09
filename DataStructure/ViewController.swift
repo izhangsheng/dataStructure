f8dadf00bac6f024d4edea35e333129536fb0f2f
zhangsheng <zhangsheng@chelun.com>
2021-07-09 17:47:52 +0800

    readme

--- DataStructure/ViewController.swift
+++ DataStructure/ViewController.swift
@@ -12,25 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        test()
-        
-        var a = 0;
-        let timer = CFRunLoopTimerCreateWithHandler(kCFAllocatorDefault, CFAbsoluteTimeGetCurrent(), 1, 0, 0) { (timer) in
-            a += 1
-            print("a是多少啊\(a)")
-            if a > 20 {
-                CFRunLoopRemoveTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.defaultMode);
-            }
-        }
-        CFRunLoopAddTimer(CFRunLoopGetCurrent(), timer, CFRunLoopMode.defaultMode);
-        
     }
-    
-    func test() {
-        let count = lcs(str1: "aocdfve", str2: "pmcdfe")
-        print(count)
-    }
-    
 }
 
